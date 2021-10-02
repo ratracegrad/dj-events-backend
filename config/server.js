fbae91a1b5241848d8daa6603b2f8660c9738705
Jennifer Bland <ratracegrad@gmail.com>
2021-10-02 16:42:21 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a6e1e2c693e94663c889024927a05db3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'af04a8c9-21c3-49d0-b694-8989bcd7251d'
+};
